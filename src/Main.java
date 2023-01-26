5fcd70c5b1763df771efd44daa0067b8f8c62417
tiaogosilva28 <tiagosilva28@live.com.pt>
2023-01-26 17:46:51 +0000

    new files

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,19 @@
 import java.io.*;
 public class Main {
 
-    public static String userAskInput (String msg){
-        InputStream in = System.in;
-        InputStreamReader reader = new InputStreamReader(in);
-
-        BufferedReader br = new BufferedReader(reader);
-        System.out.println(msg);
-        String name;
-
-        {
-            try {
-                name = br.readLine(); ///Blocking method
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        }
-        return name;
-
-    }
-
-
-
-
     public static void main(String[] args) {
 
-        Utilities.copyFile();
-        /*String name = userAskInput("What's your name mate?");
+        //Utilities.copyFile();
+       /*String name = Utilities.userAskInput("What's your name mate?");
         System.out.println("Nice to meet you: " + name);
 
-        String directory = userAskInput("What's the directory you want to see?");*/
+        String directory = Utilities.userAskInput("What's the directory you want to see?");
+        */
+
+        Utilities.copyDirectory("resources");
+
+        Utilities.askFileExistence();
+
+        Utilities.askPathExistence();
     }
 }
--- src/Utilities.java
+++ src/Utilities.java
@@ -1,8 +1,28 @@
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Utilities {
+
+    public static String userAskInput (String msg){
+        InputStream in = System.in;
+        InputStreamReader reader = new InputStreamReader(in);
+
+        BufferedReader br = new BufferedReader(reader);
+        System.out.println(msg);
+        String name;
+
+        {
+            try {
+                name = br.readLine(); ///Blocking method
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return name;
+
+    }
     protected static void copyFile() {
         int startMilliseconds = (int) System.currentTimeMillis();
 
@@ -61,7 +81,92 @@ public class Utilities {
         System.out.println("Time: " + (endMilliseconds - startMilliseconds) + "ms");
     }
 
-    protected void copyDirectory(){
+    protected static void copyDirectory(String directory){
+        FileInputStream in = null;
+        File[] files;
+        FileOutputStream out = null;
 
+        try {
+            in = new FileInputStream(directory+"/input.txt");
+            files = new File(directory).listFiles();
+            out = new FileOutputStream(directory+"/filesNames.txt");
+
+            for (int i = 0; i < files.length; i++) {
+                System.out.println(files[i].toString());
+                out.write(files[i].toString().getBytes());
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        catch (NullPointerException e){
+            System.out.println("Error: " + e.getMessage());
+        }finally {
+            try {
+                in.close();
+                out.close();
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+        }
+
+
+
+    }
+
+    protected static String askFileExistence(){
+        FileInputStream in = null;
+        File[] files;
+        FileOutputStream out = null;
+        String fileExistence = null;
+
+        String filePath = userAskInput("Whats the directory do you want to check?");
+        String fileName = userAskInput("Whats the file do you want to check?");
+
+        try {
+            files = new File(filePath).listFiles();
+
+            for (int i = 0; i < files.length; i++) {
+                if(fileName.equals(files[i].getName())){
+                    fileExistence = "File Exists";
+                    break;
+                }
+                else {
+                    fileExistence = "File doesn't Exists";
+                }
+            }
+
+        }
+        catch (NullPointerException e){
+            System.out.println("Error: " + e.getMessage());
+        }finally {
+            /*try {
+                in.close();
+                out.close();
+            } catch (IOException e) {
+                System.out.println("Error: " + e.getMessage());
+            }
+
+             */
+        }
+        System.out.println(fileExistence);
+        return fileExistence;
+    }
+
+    protected static void askPathExistence(){
+        String filePath = userAskInput("Whats the path file do you want to check?");
+        Path path = Paths.get(filePath);
+
+        try {
+            if (Files.exists(path)){
+                System.out.println("File Exists");
+            } else  {
+                System.out.println("File not found");
+            }
+
+        }
+        catch (NullPointerException e){
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
